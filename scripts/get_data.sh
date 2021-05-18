9081810a9ca91ceedefea68816c413b0e24e1df2
Nils <nholzen1@jhu.edu>
2021-05-18 03:22:55 +0200

    download and uncompress sara v2

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -2,7 +2,10 @@
 # download data
 wget https://nlp.jhu.edu/law/sara/sara.tar.gz
 # uncompress data
-tar -xzvf sara.tar.gz
-# SARA v2 TODO
+mkdir sara
+tar -xzvf sara.tar.gz -C sara
+# SARA v2
 # download data
+wget https://nlp.jhu.edu/law/sara_v2/sara_v2.tar.gz
 # uncompress data
+tar -xzvf sara_v2.tar.gz
