978b6a4f755d9b1c409e975dd63663f305d37c87
Nils <nholzen1@jhu.edu>
2021-05-24 19:23:18 +0200

    write script to get all dependencies

---
+++ scripts/get_dependencies.sh
@@ -0,0 +1,6 @@
+pip install -r requirements.txt
+bash scripts/get_data.sh
+git clone https://github.com/conll/reference-coreference-scorers.git
+wget https://nlp.stanford.edu/software/stanford-parser-full-2018-10-17.zip
+unzip stanford-parser-full-2018-10-17.zip
+echo "Download Legal Bert and unzip the file into this repository (see README.md)."
