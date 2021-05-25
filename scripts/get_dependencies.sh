27a9e0880206f566ece215ec13b88e65412c1869
SgfdDttt <nils.holzenberger@laposte.net>
2021-05-25 17:19:21 -0400

    add failure hooks

--- scripts/get_dependencies.sh
+++ scripts/get_dependencies.sh
@@ -1,6 +1,6 @@
-pip install -r requirements.txt
-bash scripts/get_data.sh
-git clone https://github.com/conll/reference-coreference-scorers.git
-wget https://nlp.stanford.edu/software/stanford-parser-full-2018-10-17.zip
-unzip stanford-parser-full-2018-10-17.zip
+pip install -r requirements.txt || exit 0
+bash scripts/get_data.sh || exit 0
+git clone https://github.com/conll/reference-coreference-scorers.git || exit 0
+wget https://nlp.stanford.edu/software/stanford-parser-full-2018-10-17.zip || exit 0
+unzip stanford-parser-full-2018-10-17.zip || exit 0
 echo "Download Legal Bert and unzip the file into this repository (see README.md)."
